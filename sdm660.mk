51a4cd988fbae354140ebee83364d05c02cdc7b8
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-25 00:19:16 +0800

    sdm660-common: Migrate to QTI USB Gadget AIDL HAL
    Change-Id: I6adbca2fab9a3fe0477d17f9786166828b786710

--- sdm660.mk
+++ sdm660.mk
@@ -317,7 +317,8 @@ PRODUCT_PACKAGES += \
 # Ramdisk-USB
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
-    init.qcom.usb.sh
+    init.qcom.usb.sh \
+    usb_compositions.conf
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
@@ -362,7 +363,8 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.basic
+    android.hardware.usb@1.3-service.basic \
+    android.hardware.usb.gadget-service.qti
 
 # Vibrator
 PRODUCT_PACKAGES += \
